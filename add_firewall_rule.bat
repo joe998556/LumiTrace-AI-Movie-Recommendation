226e0fd906fe847bbed2cf09e2a861819c0e62b2
你的名字 <GitHub>
2026-06-17 20:16:15 +0800

    chore: harden local secret ignores

--- add_firewall_rule.bat
+++ add_firewall_rule.bat
@@ -1,1 +1,10 @@
-netsh advfirewall firewall add rule name="LumiTrace BERT" protocol=TCP dir=in localport=5001 action=allow remoteip=120.107.144.231 profile=any
+@echo off
+setlocal
+
+if "%LUMITRACE_ALLOWED_PROXY_IP%"=="" (
+  echo Please set LUMITRACE_ALLOWED_PROXY_IP to the reverse proxy IP before running.
+  echo Example: set LUMITRACE_ALLOWED_PROXY_IP=YOUR_PROXY_IP
+  exit /b 1
+)
+
+netsh advfirewall firewall add rule name="LumiTrace BERT" protocol=TCP dir=in localport=5001 action=allow remoteip=%LUMITRACE_ALLOWED_PROXY_IP% profile=any
